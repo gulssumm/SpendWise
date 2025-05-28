4155ab4391e480d572c669f6bc4acc56174f6452
gulssumm <gulsumcetinozlu@gmail.com>
2025-05-29 01:34:56 +0300

    files deleted

---
+++ Data/AppDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace Data
+{
+    public class AppDataContext : DataContext
+    {
+        public AppDataContext(string connection) : base(connection) { }
+
+        public Table<User> Users => GetTable<User>();
+        public Table<Event> Events => GetTable<Event>();
+        public Table<FinancialTransaction> FinancialTransactions => GetTable<FinancialTransaction>();
+        public Table<ProcessState> ProcessStates => GetTable<ProcessState>();
+        // Add others as needed
+    }
+}
